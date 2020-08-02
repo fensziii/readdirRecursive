6697ef9a01f3e465af69c1d06c566dfa7bbc94f4
fensziii <martincry@gmail.com>
2020-08-02 13:19:47 +0200

    update

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+var rdr     = require("./../index")
+
+const path  = require("path");
+
+(async ()=>{
+
+    const options = {
+        path        : path.join(__dirname, '..'),
+        fullpath    : false,
+        filter      : "", // example ".txt" Filters for text files
+    };
+
+    const files = await rdr.readdirRecursive(options).catch(err=>console.log(err));
+
+    console.log(files);
+
+})();
