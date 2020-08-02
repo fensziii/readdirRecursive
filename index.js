5c6cce8fa132c9e76ab4faf338da596957bb52bc
fensziii <martincry@gmail.com>
2020-08-02 13:30:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ var tools = {
         // Pad to 2 or 3 digits, default is 2
         function pad(n, z) {
             z = z || 2;
-            return ('00' + n).slice(-z);
+            return ("00" + n).slice(-z);
         }
         
         var ms = s % 1000;
@@ -53,7 +53,7 @@ var tools = {
         var mins = s % 60;
         var hrs = (s - mins) / 60;
         
-        return pad(hrs) + ':' + pad(mins) + ':' + pad(secs) + '.' + pad(ms, 3);
+        return pad(hrs) + ":" + pad(mins) + ":" + pad(secs) + "." + pad(ms, 3);
 
     }
 
