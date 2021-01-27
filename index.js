83648c3ecbc924f81bb64272f603afa0c1fff29a
fensziii <martincry@gmail.com>
2021-01-27 15:27:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,62 +3,7 @@ var { Worker, isMainThread, parentPort, workerData } = require("worker_threads")
 var fs        = require("fs");
 var path      = require("path");
 var os        = require("os");
-
-/*
-
-    Tools
-
-*/
-
-var tools = {
-
-    formatBytes : function formatBytes(bytes, decimals = 2) {
-
-        /*
-
-            Source : https://stackoverflow.com/a/18650828
-
-        */
-
-        if (bytes === 0){ return "0 Bytes"; }
-    
-        var k = 1024;
-        var dm = decimals < 0 ? 0 : decimals;
-        var sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-    
-        var i = Math.floor(Math.log(bytes) / Math.log(k));
-    
-        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-
-    },
-
-    msToTime    : function msToTime(s) {
-
-        /*
-
-            Source : https://stackoverflow.com/a/9763769
-
-        */
-
-        // Pad to 2 or 3 digits, default is 2
-        function pad(n, z) {
-            z = z || 2;
-            return ("00" + n).slice(-z);
-        }
-        
-        var ms = s % 1000;
-        s = (s - ms) / 1000;
-        var secs = s % 60;
-        s = (s - secs) / 60;
-        var mins = s % 60;
-        var hrs = (s - mins) / 60;
-        
-        return pad(hrs) + ":" + pad(mins) + ":" + pad(secs) + "." + pad(ms, 3);
-
-    }
-
-};
-
+var tools     = require("./tools.js");
 
 /*
 
